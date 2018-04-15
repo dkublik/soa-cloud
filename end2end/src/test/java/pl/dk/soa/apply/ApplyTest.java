972005c90b4ce2277108fefc9abc2488bbcdd648
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-16 01:16:30 +0200

    ApplyTest-v4-async

--- end2end/src/test/java/pl/dk/soa/apply/ApplyTest.java
+++ end2end/src/test/java/pl/dk/soa/apply/ApplyTest.java
@@ -1,11 +1,19 @@
 package pl.dk.soa.apply;
 
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.person.Person;
+import io.codearte.jfairy.producer.person.PersonProperties;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.awaitility.Awaitility;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -22,40 +30,68 @@ public class ApplyTest {
     @Test
     public void shouldSuccessfullyApply() throws Exception {
         // given
-        RequestSpecification request = given()
-                .contentType(JSON)
-                .body(new JSONObject()
-                        .put("candidateId", "just_britney")
-                        .put("messageToRecruiter", "please hire me")
-                        .put("listingId", "123")
-                        .toString()
-                );
+        userIsCreatedInPrefill();
 
         // when
-        Response response = request.when().post("http://localhost:8080/v1/job-applications");
+        Response response = applyRequestForMCurie().when().post("http://localhost:8080/job-applications");
 
         // then
         response.then()
                 .statusCode(HttpStatus.ACCEPTED.value())
                 .contentType(ContentType.JSON)
                 .body("applicationId", notNullValue())
-                .body("priority", is("LOW"));
+                .body("priority", is("HIGH"));
+        assertApplicationStatus(response);
     }
 
-    // obecnie aplikacja jest tworzona z priority = "LOW",
-    // priority liczony jest ze wzgledu na staz aplikujacego
-    // yearOfExperience < 5 lat => LOW
-    // <=5 yearOfExperience < 10 => MEDIUM
-    // yearOfExperience >= 10 => HIGH
-    //
-    // przepisz test tak aby sprawdzal, ze dla uzytkownika, ze stazem powyzej 10 lat
-    // aplikacja bedzie zwracala priority: HIGH
-    // zauwaz, ze zaden z obecnych uzytkownikow
-    // http://localhost:8081/v1/prefill/for-candidate/just_britney
-    // http://localhost:8081/v1/prefill/for-candidate/mhamill
-    // http://localhost:8081/v1/prefill/for-candidate/mrpresident
-    // nie ma stazu powyzej 10 lat,
-    // aby przeprowadzic test musisz zasilic prefill-service uzytkownikiem ze stazem > 10 lat
-    // endpoint do zasilania znajdziesz przez swaggera
+    private void assertApplicationStatus(Response response) {
+        String applicationId = response.jsonPath().getString("applicationId");
+        Awaitility.await().atMost(2, TimeUnit.SECONDS).until(() -> isAccepted(applicationId));
+    }
+
+    private boolean isAccepted(String applicationId) {
+        return given()
+                .contentType("application/vnd.apply.v4+json")
+                .when()
+                .get("http://localhost:8080/job-applications/" + applicationId)
+                .jsonPath()
+                .getString("status")
+                .equals("ACCEPTED");
+    }
+
+    private RequestSpecification applyRequestForMCurie() throws JSONException {
+        return given()
+                .contentType("application/vnd.apply.v4+json")
+                .body(new JSONObject()
+                        .put("candidateId", "mcurie")
+                        .put("messageToRecruiter", "rady to work")
+                        .put("listingId", "12445")
+                        .toString()
+                );
+    }
+
+    private void userIsCreatedInPrefill() throws JSONException {
+        Person person = Fairy.create(Locale.forLanguageTag("pl")).person(PersonProperties.withUsername("mcurie"));
+        RequestSpecification request = given()
+                .contentType(JSON)
+                .body(new JSONObject()
+                        .put("dateOfBirth", person.getDateOfBirth())
+                        .put("email", person.getEmail())
+                        .put("firstName", person.getFirstName())
+                        .put("lastName", person.getLastName())
+                        .put("nationalIdentificationNumber", person.getNationalIdentificationNumber())
+                        .put("passportNumber", person.getPassportNumber())
+                        .put("phone", person.getTelephoneNumber())
+                        .put("address", new JSONObject()
+                                .put("city", person.getAddress().getCity())
+                                .put("country", "Poland")
+                                .put("street", person.getAddress().getStreet())
+                                .put("zip", person.getAddress().getPostalCode())
+                        )
+                        .put("yearOfExperience", 20)
+                        .toString()
+                );
+        request.when().put("http://localhost:8081/v1/prefill/for-candidate/mcurie");
+    }
 
 }
