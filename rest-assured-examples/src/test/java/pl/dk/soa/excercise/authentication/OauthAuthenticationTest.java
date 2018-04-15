b5052e87e369f7e70ab87c57dd8050f28de7ded1
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-15 21:22:30 +0200

    oauth exc

---
+++ rest-assured-examples/src/test/java/pl/dk/soa/excercise/authentication/OauthAuthenticationTest.java
@@ -0,0 +1,23 @@
+package pl.dk.soa.excercise.authentication;
+
+import org.junit.Test;
+
+/**
+ *  candidate-profile-service/src/main/java/pl.dk.soa.candidate/CandidateApplication must be up & running
+ *  http://localhost:8082
+ */
+public class OauthAuthenticationTest {
+
+    @Test
+    public void shouldGetDataThatRequireOauth2Authentication() {
+        // wzorujac sie na
+        // napisz test korzystajacy z autentykacji oauth2
+        //
+        // zabezpieczony przez oauth2 endpoint to "http://localhost:8082/voauth/candidates/profile"
+        //
+        // nie musisz pobierac tokenu z zewnetrznego serwisu, przyjmij, ze jest on rowny
+        // token: "1111-1111-1111"
+
+    }
+
+}
