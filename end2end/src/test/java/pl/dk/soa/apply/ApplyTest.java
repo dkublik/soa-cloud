56585db960976a7612a9f5cc0e2542074212e0b1
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-16 01:05:45 +0200

    ApplyTest-for-prefill-created

--- end2end/src/test/java/pl/dk/soa/apply/ApplyTest.java
+++ end2end/src/test/java/pl/dk/soa/apply/ApplyTest.java
@@ -3,6 +3,7 @@ package pl.dk.soa.apply;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -22,12 +23,13 @@ public class ApplyTest {
     @Test
     public void shouldSuccessfullyApply() throws Exception {
         // given
+        userIsCreatedInPrefill();
         RequestSpecification request = given()
                 .contentType(JSON)
                 .body(new JSONObject()
-                        .put("candidateId", "just_britney")
-                        .put("messageToRecruiter", "please hire me")
-                        .put("listingId", "123")
+                        .put("candidateId", "mcurie")
+                        .put("messageToRecruiter", "rady to work")
+                        .put("listingId", "12445")
                         .toString()
                 );
 
@@ -39,23 +41,30 @@ public class ApplyTest {
                 .statusCode(HttpStatus.ACCEPTED.value())
                 .contentType(ContentType.JSON)
                 .body("applicationId", notNullValue())
-                .body("priority", is("LOW"));
+                .body("priority", is("HIGH"));
     }
 
-    // obecnie aplikacja jest tworzona z priority = "LOW",
-    // priority liczony jest ze wzgledu na staz aplikujacego
-    // yearOfExperience < 5 lat => LOW
-    // <=5 yearOfExperience < 10 => MEDIUM
-    // yearOfExperience >= 10 => HIGH
-    //
-    // przepisz test tak aby sprawdzal, ze dla uzytkownika, ze stazem powyzej 10 lat
-    // aplikacja bedzie zwracala priority: HIGH
-    // zauwaz, ze zaden z obecnych uzytkownikow
-    // http://localhost:8081/v1/prefill/for-candidate/just_britney
-    // http://localhost:8081/v1/prefill/for-candidate/mhamill
-    // http://localhost:8081/v1/prefill/for-candidate/mrpresident
-    // nie ma stazu powyzej 10 lat,
-    // aby przeprowadzic test musisz zasilic prefill-service uzytkownikiem ze stazem > 10 lat
-    // endpoint do zasilania znajdziesz przez swaggera
+    private void userIsCreatedInPrefill() throws JSONException {
+        RequestSpecification request = given()
+                .contentType(JSON)
+                .body(new JSONObject()
+                        .put("dateOfBirth", "02071987")
+                        .put("email", "myEmail@candidate.com")
+                        .put("firstName", "Maria")
+                        .put("lastName", "Curie")
+                        .put("nationalIdentificationNumber", "84030115671")
+                        .put("passportNumber", "82020713898")
+                        .put("phone", "22674455")
+                        .put("address", new JSONObject()
+                                .put("city", "Szczecin")
+                                .put("country", "Polska")
+                                .put("street", "Kolorowa 12/32")
+                                .put("zip", "12-223")
+                        )
+                        .put("yearOfExperience", 20)
+                        .toString()
+                );
+        request.when().put("http://localhost:8081/v1/prefill/for-candidate/mcurie");
+    }
 
 }
