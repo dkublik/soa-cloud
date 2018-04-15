22b22384c03c3700eaa67b0ee7e28fdfcfaaf6fb
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-15 20:53:32 +0200

    deser test

---
+++ rest-assured-examples/src/test/java/pl/dk/soa/excercise/serdeser/DeserTest.java
@@ -0,0 +1,16 @@
+package pl.dk.soa.excercise.serdeser;
+
+/**
+ *  must be up & running
+ *  candidate-service/src/main/java/pl.dk.soa.candidate.CandidateApplication http://localhost:8082
+ */
+public class DeserTest {
+
+    /*
+        na podstawie DeserializationExample
+        napisac test uderzajacy do http://localhost:8082/v0/candidates/profile/{candidateId}
+        deserializujacy tylko adres z odpowiedzi
+        i assertujacy na adresie country i city
+     */
+
+}
