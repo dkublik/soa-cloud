2363dfe64ec407ed36eb8bdca04f8d1d51d584f5
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-16 19:50:37 +0200

    ApplyTest-with-wiremock

--- end2end/src/test/java/pl/dk/soa/example/stubbed/ApplyTest.java
+++ end2end/src/test/java/pl/dk/soa/example/stubbed/ApplyTest.java
@@ -1,11 +1,11 @@
 package pl.dk.soa.example.stubbed;
 
+import com.atlassian.oai.validator.restassured.SwaggerValidationFilter;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -20,11 +20,11 @@ import static org.hamcrest.CoreMatchers.notNullValue;
  *  must be up & running
  *  ats-apply-service/src/main/java/pl.dk.soa.apply.ApplyApplication http://localhost:8080
  */
-@Ignore
 public class ApplyTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(options().port(8081));
+
 
     @Test
     public void shouldSuccessfullyApply() throws Exception {
@@ -49,5 +49,23 @@ public class ApplyTest {
                 .body("priority", is("HIGH"));
     }
 
+
+    SwaggerValidationFilter validationFilter = new SwaggerValidationFilter("prefill-swagger.json");
+
+    @Test
+    public void shouldValidateAgainstOpenApiSpecification() {
+        // given:
+        RequestSpecification request = given()
+                .filter(validationFilter);
+
+        // when:
+        Response response = request.when()
+                .get("http://localhost:8081/v1/prefill/for-candidate/just_britney");
+
+        // then
+        response.then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+    }
 
 }
