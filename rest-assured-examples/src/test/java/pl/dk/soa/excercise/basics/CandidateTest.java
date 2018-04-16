622483170fe99c38049f31c81d91042effeb862d
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-17 00:35:07 +0200

    cdn-ex

--- rest-assured-examples/src/test/java/pl/dk/soa/excercise/basics/CandidateTest.java
+++ rest-assured-examples/src/test/java/pl/dk/soa/excercise/basics/CandidateTest.java
@@ -1,5 +1,10 @@
 package pl.dk.soa.excercise.basics;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 /**
@@ -10,25 +15,25 @@ public class CandidateTest {
 
     @Test
     public void shouldGetProfileDetails() {
-        // given / when
-        // test zasobu: GET http://localhost:8082/v0/candidates/profile/{candidateId}
-        // testowany uzytkownik mHamill
+        // given
+        RequestSpecification requestSpecification = RestAssured.given()
+                .pathParam("candidateId", "mHamill");
+
+        // when
+        Response response = requestSpecification.when().get( "http://localhost:8082/v0/candidates/profile/{candidateId}");
 
         // then
-        // do sprawdzenia co najmniej:
-        // status odpowiedzi
-        // content type odpowiedzi
-        // login
-        // name
-        // surname
-        // email
-        // address.country
-        // address.city
-        // address.street
+        response.then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("login", CoreMatchers.is("mhamill"))
+                .body("name", CoreMatchers.is("Mark"))
+                .body("surname", CoreMatchers.is("Hamill"))
+                .body("email", CoreMatchers.is("mark.hamill@gmail.com"))
+                .body("address.country", CoreMatchers.is("Poland"))
+                .body("address.city", CoreMatchers.is("Warsaw"))
+                .body("address.street", CoreMatchers.is("Zaruby 10"));
 
-
-        // hint:
-        // odpowiedz mozna podejrzec pod http://localhost:8082/swagger-ui.html
     }
 
 }
